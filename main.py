698def3ac97a1e283670390c27a0366256625114
Het Raval <hetraval3333@gmail.com>
2025-02-23 22:18:25 +0530

    added

--- main.py
+++ main.py
@@ -157,4 +157,7 @@ def upload_photo():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=3000)
+    socketio.run(app, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment, default to 5000
+    app.run(host='0.0.0.0', port=port)
